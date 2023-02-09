76712bc674d47ccd002472077d0eec773c97585c
ANANT-2004 <74396966+ANANT-2004@users.noreply.github.com>
2023-02-09 15:58:28 +0530

    Add files via upload

---
+++ test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void squareint();
+
+int main(){
+    void squareeint();
+
+    return 0;
+    
+    }
+
+void squareint(){
+    int a,b;
+    printf("enter number");
+    scanf("%d",&a);
+    b=a*a;
+}
+  
